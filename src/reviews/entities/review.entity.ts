93d6d78ecc50cb8b70be57e137b9ebdd2c203d3d
han <handv0412@gmail.com>
2025-08-22 15:51:39 +0900

    feat: entity 작성 review, review-comment

--- src/reviews/entities/review.entity.ts
+++ src/reviews/entities/review.entity.ts
@@ -1,1 +1,19 @@
-export class Review {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('review') // DB 테이블 이름
+export class Review {
+  @PrimaryGeneratedColumn()
+  review_id: number; // 리뷰 id
+
+  @Column({ type: 'varchar', length: 50 })
+  rating: string; // 매장별점 (String)
+
+  @Column({ type: 'text' })
+  review_contents: string; // 리뷰내용
+
+  @Column({ type: 'varchar', length: 100 })
+  review_create_time: string; // 리뷰생성날짜 (문자열 저장)
+
+  @Column({ type: 'boolean', default: false })
+  review_cancel: boolean; // 리뷰삭제여부
+}
