0c73c9758c8ef95b5f3d1264bfab2e10dc3bb71d
forhn37 <forhn37@gmail.com>
2025-08-28 18:19:04 +0900

    수정적용

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,18 +1,7 @@
-import { IsNotEmpty, IsNumber, IsString, Min, Max } from 'class-validator';
-
 // src/reviews/dto/create-review.dto.ts
 export class CreateReviewDto {
-  @IsNotEmpty()
-  @IsNumber()
+  storeId: number; // 매장 ID
   reservationId: number; // 예약 ID
-
-  @IsNotEmpty()
-  @IsNumber()
-  @Min(1)
-  @Max(5)
-  rating: number; // 별점 (1-5)
-
-  @IsNotEmpty()
-  @IsString()
-  content: string; // 리뷰 내용 (프론트엔드에서는 reviewText로 보내지만 content로 받음)
+  rating: string; // 매장별점
+  content: string; // 리뷰 내용
 }
